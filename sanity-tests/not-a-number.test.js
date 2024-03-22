d4451581633b8bbedffa90075ea84d38c605f4c8
WriteRead024 <rich@rwhifonline.net>
2024-03-22 16:29:33 -0400

    not a number tests

---
+++ sanity-tests/not-a-number.test.js
@@ -0,0 +1,34 @@
+
+// not-a-number.test.js
+// started 3/22/2024
+// Rich W.
+// with
+// GitHub Copilot
+
+test('NaN equals NaN is false', () => {
+    expect(NaN == NaN).toBe(false);
+});
+
+test('NaN strictly equals NaN is false', () => {
+    expect(NaN === NaN).toBe(false);
+});
+
+test('parseFloat("string") is NaN', () => {
+    expect(parseFloat("string")).toBe(NaN);
+});
+
+test('parseInt("string") is NaN', () => {
+    expect(parseInt("string")).toBe(NaN);
+});
+
+test('typeof NaN is number', () => {
+    expect(typeof NaN).toBe('number');
+});
+
+test('isNaN(parseInt("string")) is true', () => {
+    expect(isNaN(parseInt("string"))).toBe(true);
+});
+
+test('isNaN(Number.NaN) is true', () => {
+    expect(isNaN(Number.NaN)).toBe(true);
+});
