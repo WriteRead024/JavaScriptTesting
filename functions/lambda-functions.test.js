0bd3a45bf9e80fe33ad3546f231ba9b01debca20
WriteRead024 <rich@rwhifonline.net>
2024-04-21 10:04:19 -0400

    lambda functions tests

---
+++ functions/lambda-functions.test.js
@@ -0,0 +1,20 @@
+describe('Lambda functions', () => {
+    test('should be able to have properties', () => {
+        let lambda = () => {};
+        lambda.prop = 'value';
+        expect(lambda.prop).toBe('value');
+    });
+
+    test('should be able to access its own properties', () => {
+        let lambda = () => lambda.prop;
+        lambda.prop = 'value';
+        expect(lambda()).toBe('value');
+    });
+
+    test('should be able to access its own properties in the method', () => {
+        let lambda = function() { return "property " + this.prop; };
+        expect(lambda.call(lambda)).toBe('property undefined');
+        lambda.prop = 'value';
+        expect(lambda.call(lambda)).toBe('property value');
+    });
+});
