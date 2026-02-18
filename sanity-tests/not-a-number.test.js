1298ef7ee4bf664e571b6e311bf4e907d537b167
WriteRead024 <rich@rwhifonline.net>
2026-02-18 11:12:11 -0500

    NaN test additions

--- sanity-tests/not-a-number.test.js
+++ sanity-tests/not-a-number.test.js
@@ -1,6 +1,7 @@
 
 // not-a-number.test.js
 // started 3/22/2024
+// additons 2/18/2026
 // Rich W.
 // with
 // GitHub Copilot
@@ -13,12 +14,16 @@ test('NaN strictly equals NaN is false', () => {
     expect(NaN === NaN).toBe(false);
 });
 
+test('parseInt("string") is NaN', () => {
+    expect(parseInt("string")).toBe(NaN);
+});
+
 test('parseFloat("string") is NaN', () => {
     expect(parseFloat("string")).toBe(NaN);
 });
 
-test('parseInt("string") is NaN', () => {
-    expect(parseInt("string")).toBe(NaN);
+test('parseInt("123") is number', () => {
+    expect(parseInt("123")).toBe(123);
 });
 
 test('typeof NaN is number', () => {
@@ -33,8 +38,8 @@ test('isNaN(Number.NaN) is true', () => {
     expect(isNaN(Number.NaN)).toBe(true);
 });
 
-test('isNaN(parseInt("string")) is true', () => {
-    expect(isNaN(parseInt("string"))).toBe(true);
+test('isNaN(parseInt("123")) is false', () => {
+    expect(isNaN(parseInt("123"))).toBe(false);
 });
 
 test('NaN is NaN', () => {
